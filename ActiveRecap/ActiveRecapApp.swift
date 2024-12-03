a6df7f7e088c292e20f46b74beb2a15ff35cd6aa
Nick <nickvmorello@gmail.com>
2024-12-02 23:31:24 -0700

    Initial Commit

---
+++ ActiveRecap/ActiveRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveRecapApp.swift
+//  ActiveRecap
+//
+//  Created by Nick Morello on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ActiveRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActiveRecapTests/ActiveRecapTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveRecapTests.swift
+//  ActiveRecapTests
+//
+//  Created by Nick Morello on 12/2/24.
+//
+
+import Testing
+@testable import ActiveRecap
+
+struct ActiveRecapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
